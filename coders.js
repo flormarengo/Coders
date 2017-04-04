eec96ac89c7f9fef4e88a5d5516f4277014ae586
flormarengo <flor9312@gmail.com>
2017-04-04 14:06:38 -0500

    coders

---
+++ coders.js
@@ -0,0 +1,14 @@
+
+
+var select = document.getElementById("select");
+
+select.addEventListener("onchange",function(){
+  if(select.cuartaPromo.value == ""){
+    alert("selecciona un campo");
+  }
+})
+
+select.onchange = function(){
+  if(select.value == ""){
+    alert("fvfnv");
+  })
